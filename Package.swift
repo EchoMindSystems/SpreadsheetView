d3196f71d1a773be1d7bd3976dd14b4e7eb94a10
Can <cankocoglu@gmail.com>
2024-11-02 03:32:25 +0530

    initial commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SpreadsheetView",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SpreadsheetView",
+            targets: ["SpreadsheetViewKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SpreadsheetView"),
+        .testTarget(
+            name: "SpreadsheetViewTests",
+            dependencies: ["SpreadsheetView"]
+        ),
+		.binaryTarget(
+			name: "SpreadsheetViewKit",
+			path: "./XCFramework/SpreadsheetViewKit.xcframework"
+		),
+    ]
+)
---
+++ XCFramework/SpreadsheetViewKit.xcframework/ios-arm64/SpreadsheetViewKit.framework/Headers/SpreadsheetViewKit.h
@@ -0,0 +1,18 @@
+//
+//  SpreadsheetViewKit.h
+//  SpreadsheetViewKit
+//
+//  Created by Arbab Ahmed Khan on 02/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpreadsheetViewKit.
+FOUNDATION_EXPORT double SpreadsheetViewKitVersionNumber;
+
+//! Project version string for SpreadsheetViewKit.
+FOUNDATION_EXPORT const unsigned char SpreadsheetViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpreadsheetViewKit/PublicHeader.h>
+
+
